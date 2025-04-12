2e7b38ac04dcfcfaf1c45e08f530c11c72051a12
allenmylath <allengeorgemylath@gmail.com>
2025-04-12 11:03:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,8 @@
 # SPDX-License-Identifier: BSD 2-Clause License
 #
 
-import datetime
-import io
 import os
 import sys
-import wave
-
-import aiofiles
 from dotenv import load_dotenv
 from fastapi import WebSocket
 from loguru import logger
@@ -20,25 +15,29 @@ from pipecat.pipeline.pipeline import Pipeline
 from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineParams, PipelineTask
 from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
-from pipecat.processors.audio.audio_buffer_processor import AudioBufferProcessor
 from pipecat.serializers.twilio import TwilioFrameSerializer
 from pipecat.services.cartesia import CartesiaTTSService
+from pipecat.processors.audio.audio_buffer_processor import AudioBufferProcessor
 from pipecat.services.deepgram import DeepgramSTTService
 from pipecat.services.openai import OpenAILLMService
+from pipecat.services.cerebras import CerebrasLLMService
 from pipecat.transports.network.fastapi_websocket import (
     FastAPIWebsocketParams,
     FastAPIWebsocketTransport,
 )
+from audio_s3 import save_audio_to_s3
 
 load_dotenv(override=True)
+
 
 logger.remove(0)
 logger.add(sys.stderr, level="DEBUG")
 
 
-
-
-async def run_bot(websocket_client: WebSocket, stream_sid: str,):
+async def run_bot(
+    websocket_client: WebSocket,
+    stream_sid: str,
+):
     transport = FastAPIWebsocketTransport(
         websocket=websocket_client,
         params=FastAPIWebsocketParams(
@@ -54,27 +53,41 @@ async def run_bot(websocket_client: WebSocket, stream_sid: str,):
 
     llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o-mini")
 
-    stt = DeepgramSTTService(api_key=os.getenv("DEEPGRAM_API_KEY"), audio_passthrough=True)
+    llm = CerebrasLLMService(
+        # api_key=os.getenv("CEREBRAS_API_KEY"), model="llama-3.3-70b"
+        api_key=os.getenv("CEREBRAS_API_KEY"),
+        model="llama-4-scout-17b-16e-instruct",
+    )
+
+    stt = DeepgramSTTService(
+        api_key=os.getenv("DEEPGRAM_API_KEY"), audio_passthrough=True
+    )
 
     tts = CartesiaTTSService(
-          api_key=os.getenv("CARTESIA_API_KEY"),
-          voice_id="71a7ad14-091c-4e8e-a314-022ece01c121",  # British Reading Lady
-          params=CartesiaTTSService.InputParams(
-              speed="normal",  # Using the "slow" label as specified in the API
-              emotion=["positivity", "curiosity"]  # Adding high positivity and moderate curiosity
-                 )
-          )
+        api_key=os.getenv("CARTESIA_API_KEY"),
+        voice_id="156fb8d2-335b-4950-9cb3-a2d33befec77",
+        params=CartesiaTTSService.InputParams(
+            speed="normal",
+            emotion=[
+                "positivity",
+                "curiosity",
+            ],  # Adding high positivity and moderate curiosity
+        ),
+    )
+    text = """
+    YOU ARE JESSICA. your output is connected to audio. so try to punctuate the sentences so that tts model can give emotions in voice. try to speak as if a human would speak.
+Use appropriate punctuation. Use short sentences.Dont be chatty.Add punctuation where appropriate and at the end of each transcript whenever possible.
+Use dates in MM/DD/YYYY form.
+To insert pauses, insert “-”. dont use ... for pauses.
+"""
 
     messages = [
-        {
-            "role": "system",
-            "content": "You are helpful lady named Jessicca. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a short short sentence.",
-        },
+        {"role": "system", "content": text},
     ]
 
     context = OpenAILLMContext(messages)
     context_aggregator = llm.create_context_aggregator(context)
-
+    audiobuffer = AudioBufferProcessor()
 
     pipeline = Pipeline(
         [
@@ -84,6 +97,7 @@ async def run_bot(websocket_client: WebSocket, stream_sid: str,):
             llm,  # LLM
             tts,  # Text-To-Speech
             transport.output(),  # Websocket output to client
+            audiobuffer,
             context_aggregator.assistant(),
         ]
     )
@@ -91,28 +105,52 @@ async def run_bot(websocket_client: WebSocket, stream_sid: str,):
     task = PipelineTask(
         pipeline,
         params=PipelineParams(
-            #audio_in_sample_rate=8000,
-            #audio_out_sample_rate=8000,
+            # audio_in_sample_rate=8000,
+            # audio_out_sample_rate=8000,
             allow_interruptions=True,
         ),
     )
 
+    @audiobuffer.event_handler("on_audio_data")
+    async def on_audio_data(buffer, audio, sample_rate, num_channels):
+        try:
+            print("starting upload")
+            s3_url = await save_audio_to_s3(
+                audio=audio,
+                sample_rate=sample_rate,
+                num_channels=num_channels,
+                bucket_name="careadhdaudio",
+            )
+            logger.info(
+                f"Successfully saved {len(audio)} bytes of audio to S3.{s3_url}"
+            )
+
+        except Exception as e:
+            logger.error(f"Error saving audio to S3: {e}")
+
     @transport.event_handler("on_client_connected")
     async def on_client_connected(transport, client):
+        await audiobuffer.start_recording()
+        print("Recording started")
+
         # Kick off the conversation.
-        messages.append({"role": "system", "content": "Please introduce yourself as jessica to the user and ask how you can help them."})
+        messages.append(
+            {
+                "role": "system",
+                "content": "Please introduce yourself as jessica to the user and ask how you can help them.",
+            }
+        )
         await task.queue_frames([context_aggregator.user().get_context_frame()])
 
     @transport.event_handler("on_client_disconnected")
     async def on_client_disconnected(transport, client):
+        await audiobuffer.stop_recording()
         await task.cancel()
-
-
 
     # We use `handle_sigint=False` because `uvicorn` is controlling keyboard
     # interruptions. We use `force_gc=True` to force garbage collection after
     # the runner finishes running a task which could be useful for long running
     # applications with multiple clients connecting.
-    runner = PipelineRunner(handle_sigint=False, force_gc=True)
+    runner = PipelineRunner(handle_sigint=True, force_gc=True)
 
     await runner.run(task)
